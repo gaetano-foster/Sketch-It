d142dee91e62758b0d5680f8cbb937155c12aaf5
ShadowCaesar701 <70331267+ShadowCaesar701@users.noreply.github.com>
2021-06-02 18:52:00 -0400

    .

--- src/sketch_it/io/display/Display.java
+++ src/sketch_it/io/display/Display.java
@@ -38,4 +38,54 @@ public class Display
         frame.add(canvas);
         frame.pack();
     }
+
+    public int getWidth()
+    {
+        return width;
+    }
+
+    public void setWidth(int width)
+    {
+        this.width = width;
+    }
+
+    public int getHeight()
+    {
+        return height;
+    }
+
+    public void setHeight(int height)
+    {
+        this.height = height;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        frame.setTitle(title);
+    }
+
+    public JFrame getFrame()
+    {
+        return frame;
+    }
+
+    public void setFrame(JFrame frame)
+    {
+        this.frame = frame;
+    }
+
+    public Canvas getCanvas()
+    {
+        return canvas;
+    }
+
+    public void setCanvas(Canvas canvas)
+    {
+        this.canvas = canvas;
+    }
 }
--- src/sketch_it/main/App.java
+++ src/sketch_it/main/App.java
@@ -1,5 +1,88 @@
 package sketch_it.main;
 
+import sketch_it.io.display.Display;
+
 public class App
 {
+    // display
+    private int width, height;
+    private String title;
+    private Display display;
+
+    // loop
+    private boolean running = false;
+
+    public App(int width, int height, String title)
+    {
+        this.width = width;
+        this.height = height;
+        this.title = title;
+    }
+
+    private void init()
+    {
+        display = new Display(width, height, title);
+    }
+
+    private void run()
+    {
+        init();
+
+        double delta = 0;
+        int desiredFPS = 240;
+        double timePerTick = 1000000000 / desiredFPS;
+        long now;
+        long lastTime = System.nanoTime();
+        long timer = 0;
+        float ticks = 0;
+
+        while (running)
+        {
+            now = System.nanoTime();
+            delta += (now - lastTime) / timePerTick;
+            timer += now - lastTime;
+            lastTime = now;
+
+            if (delta >= 1)
+            {
+                update();
+                render();
+                ticks++;
+                delta--;
+            }
+
+            if (timer >= 1000000000)
+            {
+                display.setTitle(display.getTitle() + " FPS: " + ticks);
+                ticks = 0;
+                timer = 0;
+            }
+        }
+
+    }
+
+    private void update()
+    {
+
+    }
+
+    private void render()
+    {
+
+    }
+
+    public synchronized void start()
+    {
+        if (running)
+            return;
+        running = true;
+        run();
+    }
+
+    public synchronized void stop()
+    {
+        if (!running)
+            return;
+        running = false;
+    }
 }
